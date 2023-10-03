2d6c643d7e3311a5e8097f367484c19a5e57b573
TruongTanNghia <tannghiaxp@gmail.com>
2023-10-04 02:54:02 +0700

    #3 Update Save file video the folder

--- Project 1 - Car Counter/Car-Counter.py
+++ Project 1 - Car Counter/Car-Counter.py
@@ -4,8 +4,21 @@ import cv2
 import cvzone
 import math
 from sort import *
+import os
 
-cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Video
+# Video input/output paths
+video_path = "../Videos/cars.mp4"
+output_folder = "output_folder"  # Change the output folder as needed
+output_video_name = "output_video.avi"
+
+# Create the output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Define the output video path
+output_video_path = os.path.join(output_folder, output_video_name)
+
+cap = cv2.VideoCapture(video_path)  # For Video
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
 
@@ -29,8 +42,15 @@ tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 limits = [400, 297, 673, 297]
 totalCount = []
 
+# Define the codec and create a VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Adjust codec as needed
+out = cv2.VideoWriter(output_video_path, fourcc, 20.0, (1280, 720))  # Adjust frame size as needed
+
 while True:
     success, img = cap.read()
+    if not success:
+        break
+
     imgRegion = cv2.bitwise_and(img, mask)
 
     imgGraphics = cv2.imread("graphics.png", cv2.IMREAD_UNCHANGED)
@@ -45,7 +65,6 @@ while True:
             # Bounding Box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
             w, h = x2 - x1, y2 - y1
 
             # Confidence
@@ -56,9 +75,6 @@ while True:
 
             if currentClass == "car" or currentClass == "truck" or currentClass == "bus" \
                     or currentClass == "motorbike" and conf > 0.3:
-                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0, x1), max(35, y1)),
-                #                    scale=0.6, thickness=1, offset=3)
-                # cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
                 currentArray = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, currentArray))
 
@@ -68,7 +84,6 @@ while True:
     for result in resultsTracker:
         x1, y1, x2, y2, id = result
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        print(result)
         w, h = x2 - x1, y2 - y1
         cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
         cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)),
@@ -82,9 +97,16 @@ while True:
                 totalCount.append(id)
                 cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
 
-    # cvzone.putTextRect(img, f' Count: {len(totalCount)}', (50, 50))
-    cv2.putText(img,str(len(totalCount)),(255,100),cv2.FONT_HERSHEY_PLAIN,5,(50,50,255),8)
+    cv2.putText(img, str(len(totalCount)), (255, 100), cv2.FONT_HERSHEY_PLAIN, 5, (50, 50, 255), 8)
 
     cv2.imshow("Image", img)
-    # cv2.imshow("ImageRegion", imgRegion)
-    cv2.waitKey(1)
+    # Write the frame to the video
+    out.write(img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release resourcesq
+cap.release()
+out.release()
+cv2.destroyAllWindows()
