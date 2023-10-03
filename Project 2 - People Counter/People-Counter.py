b52a75c2a378cab45badcd5280250bf4f7eef9f6
TruongTanNghia <tannghiaxp@gmail.com>
2023-10-04 02:12:40 +0700

    #4 People Counter

---
+++ Project 2 - People Counter/People-Counter.py
@@ -0,0 +1,114 @@
+import numpy as np
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+from sort import *
+
+cap = cv2.VideoCapture("../Videos/people.mp4")  # For Video
+
+model = YOLO("../Yolo-Weights/yolov8l.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+mask = cv2.imread("mask.png")
+
+# Tracking
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+limitsUp = [103, 161, 296, 161]
+limitsDown = [527, 489, 735, 489]
+
+totalCountUp = []
+totalCountDown = []
+
+# Tạo thư mục để lưu video (đảm bảo thư mục tồn tại)
+output_folder = 'output_videos'
+os.makedirs(output_folder, exist_ok=True)
+
+# Tạo VideoWriter để xuất video
+output_path = os.path.join(output_folder, 'output_video.avi')
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+output_video = cv2.VideoWriter(output_path, fourcc, 20.0, (int(cap.get(3)), int(cap.get(4))))
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    imgRegion = cv2.bitwise_and(img, mask)
+    imgGraphics = cv2.imread("graphics.png", cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img, imgGraphics, (730, 260))
+    results = model(imgRegion, stream=True)
+
+    detections = np.empty((0, 5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+
+            if currentClass == "person" and conf > 0.3:
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 0, 255), 5)
+    cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 0, 255), 5)
+
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2 - x1, y2 - y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
+        cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)),
+                           scale=2, thickness=3, offset=10)
+
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
+        if limitsUp[0] < cx < limitsUp[2] and limitsUp[1] - 15 < cy < limitsUp[1] + 15:
+            if totalCountUp.count(id) == 0:
+                totalCountUp.append(id)
+                cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 255, 0), 5)
+
+        if limitsDown[0] < cx < limitsDown[2] and limitsDown[1] - 15 < cy < limitsDown[1] + 15:
+            if totalCountDown.count(id) == 0:
+                totalCountDown.append(id)
+                cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 255, 0), 5)
+
+    # Ghi khung hình vào video
+    output_video.write(img)
+
+    # Hiển thị ảnh với các bounding box và thông tin
+    cv2.imshow("Image", img)
+
+    # Thoát khỏi vòng lặp nếu nhấn 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Giải phóng tài nguyên
+output_video.release()
+cv2.destroyAllWindows()
+
+print('Video exported successfully:', output_path)
